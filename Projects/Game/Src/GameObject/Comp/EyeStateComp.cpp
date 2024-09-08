4850f77566dbf50122a3e773f38c70267d6d24bd
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-09-08 22:55:05 +0900

    状態の追加。変数分け。getterの追加

--- Projects/Game/Src/GameObject/Comp/EyeStateComp.cpp
+++ Projects/Game/Src/GameObject/Comp/EyeStateComp.cpp
@@ -2,13 +2,28 @@
 
 void EyeStateComp::Save(nlohmann::json& json) {
 	SaveCompName(json);
-	json["aimeTime"] = aimTime;
-	json["aimFixedTime"] = aimFixedTime;
-	json["fireTime"] = fireTime;
+	json["aimeTime"] = aimTime_;
+	json["aimFixedTime"] = aimFixedTime_;
+	json["fireTime"] = fireTime_;
 }
 
 void EyeStateComp::Load(nlohmann::json& json) {
-	aimTime = json["aimeTime"].get<float32_t>();
-	aimFixedTime = json["aimFixedTime"].get<float32_t>();
-	fireTime = json["fireTime"].get<float32_t>();
+	aimTime_ = json["aimeTime"].get<float32_t>();
+	aimFixedTime_ = json["aimFixedTime"].get<float32_t>();
+	fireTime_ = json["fireTime"].get<float32_t>();
+}
+
+float32_t EyeStateComp::GetAimTime() const
+{
+	return aimTime_;
+}
+
+float32_t EyeStateComp::GetAimFixedTime() const
+{
+	return aimFixedTime_;
+}
+
+float32_t EyeStateComp::GetFireTime() const
+{
+	return fireTime_;
 }
--- Projects/Game/Src/GameObject/Comp/EyeStateComp.h
+++ Projects/Game/Src/GameObject/Comp/EyeStateComp.h
@@ -3,10 +3,11 @@
 class EyeStateComp : public IComp {
 public:
 	enum class State {
-		kSerch,
+		kSearch,
 		kAim,
+		kAimFixed,
 		kFire,
-		
+
 		kNum
 	};
 
@@ -20,13 +21,26 @@ public:
 	void Load(nlohmann::json& json) override;
 
 public:
-	// 狙っている時間
-	float32_t aimTime = 0.0f;
-	// 狙いを定めて狙いを固定している時間
-	float32_t aimFixedTime = 0.0f;
-	// 発射している時間(この時間は当たり判定が発生？)
-	float32_t fireTime = 0.0f;
+	float32_t GetAimTime() const;
+	float32_t GetAimFixedTime() const;
+	float32_t GetFireTime() const;
+
+public:
+	// 狙っている時間のカウント
+	float32_t aimCount = 0.0f;
+	// 狙いを定めて狙いを固定している時間のカウント
+	float32_t aimFixedCount = 0.0f;
+	// 発射している時間のカウント(この間は当たり判定が発生？)
+	float32_t fireCount = 0.0f;
 
 	// 初期の状態は探している状態
-	State state = State::kSerch;
+	State state = State::kSearch;
+
+private:
+	// 狙っている時間
+	float32_t aimTime_ = 0.0f;
+	// 狙いを定めて狙いを固定している時間
+	float32_t aimFixedTime_ = 0.0f;
+	// 発射している時間(この時間は当たり判定が発生？)
+	float32_t fireTime_ = 0.0f;
 };
