1cca2fd29fd58ca6ca5e54f2c70d6d30faeab2d8
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-09-08 22:55:31 +0900

    状態の変化による処理の追加

---
+++ Projects/Game/Src/GameObject/Comp/EyeComp.cpp
@@ -0,0 +1,121 @@
+#include "EyeComp.h"
+#include "PlayerComp.h"
+#include "TransformComp.h"
+#include "LineComp.h"
+#include "LineCollisionComp.h"
+#include "EyeStateComp.h"
+
+void EyeComp::Init()
+{
+	transformComp_ = object_.AddComp<TransformComp>();
+	beamLineComp_ = object_.AddComp<LineComp>();
+	beamLineCollisionComp_ = object_.AddComp<LineCollisionComp>();
+	eyeStateComp_ = object_.AddComp<EyeStateComp>();
+}
+
+void EyeComp::SetPlayerComp(PlayerComp* playerComp) {
+	playerComp_ = playerComp;
+	playerTransformComp_ = playerComp_->getObject().GetComp<TransformComp>();
+	beamLineComp_->end = playerTransformComp_->translate;
+
+	playerObbComp_ = playerComp_->getObject().GetComp<ObbComp>();
+}
+
+void EyeComp::Save(nlohmann::json& json) {
+	SaveCompName(json);
+}
+
+void EyeComp::Load([[maybe_unused]]nlohmann::json& json) {
+}
+
+void EyeComp::FirstUpdate() {
+	// ビームの始まりをEyeのポジションにする
+	beamLineComp_->start = transformComp_->translate;
+	// ビームの終わりをプレイヤーにして当たり判定をとる
+	// もしエイムを固定してるか発射中ならなにもしない
+	if (eyeStateComp_->state != EyeStateComp::State::kAimFixed and eyeStateComp_->state != EyeStateComp::State::kFire) {
+		beamLineComp_->end = playerTransformComp_->translate;
+	}
+}
+
+void EyeComp::Event() {
+
+	// 当たっていたらポインタを持っている
+	// 持っていなかったらプレイヤーを直視できてる
+	bool isCollision = false;
+	Lamb::SafePtr mostNearCollisionObjectPtr = beamLineCollisionComp_->GetMostNearCollisionObjectPtr();
+	if (mostNearCollisionObjectPtr.have()) {
+		Lamb::SafePtr collsionCbjectTransFormComp = mostNearCollisionObjectPtr->GetComp<TransformComp>();
+		// 長さを当たっているオブジェクトの長さにする
+		beamLineComp_->end = beamLineComp_->GetDirection() * (collsionCbjectTransFormComp->translate - beamLineComp_->start).Length();
+		isCollision = true;
+	}
+
+	// 状態
+	switch (eyeStateComp_->state) {
+		// 探してる
+	case EyeStateComp::State::kSearch:
+		// 探している最中にプレイヤーを直視できたら狙う
+		if(isCollision){
+			eyeStateComp_->state = EyeStateComp::State::kAim;
+		}
+
+		// 初期化
+		eyeStateComp_->aimCount = 0.0f;
+		// 初期化
+		eyeStateComp_->aimFixedCount = 0.0f;
+		// 初期化
+		eyeStateComp_->fireCount = 0.0f;
+
+		break;
+		// 狙いを定める
+	case EyeStateComp::State::kAim:
+		// 狙いを定めている最中にプレイヤーを直視できなくなったらSearchに移行
+		if(isCollision){
+			eyeStateComp_->state = EyeStateComp::State::kSearch;
+		}
+
+		// ビームの終わりをプレイヤーにする
+		beamLineComp_->end = playerTransformComp_->translate;
+
+		// 時間を加算
+		eyeStateComp_->aimCount += object_.GetDeltaTime();
+
+		// 狙いを定めてる最中の時間を超えたら固定
+		if (eyeStateComp_->GetAimTime() <= eyeStateComp_->aimCount) {
+			eyeStateComp_->state = EyeStateComp::State::kAimFixed;
+		}
+
+		break;
+		// 狙いを固定
+	case EyeStateComp::State::kAimFixed:
+		// 時間を加算
+		eyeStateComp_->aimFixedCount += object_.GetDeltaTime();
+
+		// 固定時間を超えたら発射
+		if (eyeStateComp_->GetAimFixedTime() <= eyeStateComp_->aimFixedCount) {
+			eyeStateComp_->state = EyeStateComp::State::kFire;
+		}
+
+		break;
+		// 発射
+	case EyeStateComp::State::kFire:
+		// 時間を加算
+		eyeStateComp_->fireCount += object_.GetDeltaTime();
+
+		bool isPlayerComp = playerObbComp_->IsCollision(beamLineComp_->start, beamLineComp_->end);
+
+		playerComp_->SetIsBeamCollision(isPlayerComp);
+
+		if (eyeStateComp_->GetFireTime() <= eyeStateComp_->fireCount) {
+			eyeStateComp_->state = EyeStateComp::State::kSearch;
+		}
+
+		break;
+	default:
+		// ここに入ったらエラー
+		throw Lamb::Error::Code<EyeComp>("State error", ErrorPlace);
+		break;
+	}
+
+}
--- Projects/Game/Src/GameObject/Comp/EyeComp.h
+++ Projects/Game/Src/GameObject/Comp/EyeComp.h
@@ -9,13 +9,31 @@ public:
 public:
 	void Init() override;
 
+	void SetPlayerComp(class PlayerComp* playerComp);
+
+	void Save(nlohmann::json& json) override;
+	void Load(nlohmann::json& json) override;
+
+	void FirstUpdate() override;
+	// 当たり判定後の処理
+	void Event() override;
+
 
 private:
 	// これは別のオブジェクトが持っているPlayerComp
 	Lamb::SafePtr<class PlayerComp> playerComp_;
+	// プレイヤーのTransformComp
+	Lamb::SafePtr<class TransformComp> playerTransformComp_;
+	Lamb::SafePtr<class ObbComp> playerObbComp_;
 
+private:
 	Lamb::SafePtr<class TransformComp> transformComp_;
 
+	// 黒目の子オブジェクト(これはベータ版で使う)
+	Lamb::SafePtr<class ChildrenComp> eyeBlackEye_;
 
-	Lamb::SafePtr<class ChildrenComp> eyeLine_;
+	Lamb::SafePtr<class LineComp> beamLineComp_;
+	Lamb::SafePtr<class LineCollisionComp> beamLineCollisionComp_;
+
+	Lamb::SafePtr<class EyeStateComp> eyeStateComp_;
 };
