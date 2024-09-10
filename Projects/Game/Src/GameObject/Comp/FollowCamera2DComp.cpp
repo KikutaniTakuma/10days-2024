97495f8df78a7e6372c054c869d61bd030187b5a
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2024-09-10 15:29:30 +0900

    カメラ追従、エディター更新、判定改善

--- Projects/Game/Src/GameObject/Comp/FollowCamera2DComp.cpp
+++ Projects/Game/Src/GameObject/Comp/FollowCamera2DComp.cpp
@@ -14,7 +14,27 @@ void FollowCamera2DComp::Move()
 
 	if (target_) {
 
-		
+		//ターゲットの座標が範囲内なら追従
+		if (targetTransform_->translate.x > min_.x and max_.x > targetTransform_->translate.x) {
+			transform_->translate.x = targetTransform_->translate.x;
+		}
+		else if(targetTransform_->translate.x <= min_.x) {
+			transform_->translate.x = min_.x;
+		}
+		else {
+			transform_->translate.x = max_.x;
+		}
+
+		////ターゲットの座標が範囲内なら追従
+		//if (targetTransform_->translate.y > min_.y and max_.y > targetTransform_->translate.y) {
+		//	transform_->translate.y = targetTransform_->translate.y;
+		//}
+		//else if (targetTransform_->translate.y <= min_.y) {
+		//	transform_->translate.y = min_.y;
+		//}
+		//else {
+		//	transform_->translate.y = max_.y;
+		//}
 
 	}
 
@@ -28,7 +48,7 @@ void FollowCamera2DComp::SetPlayerComp(PlayerComp* playerComp)
 		return;
 	}
 
-
+	targetTransform_ = target_->getObject().GetComp<TransformComp>();
 
 }
 
